54f32a2cf83d6d7a33c379a5b7c47756106314b7
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-11-30 21:56:17 +0100

    Added Animal example file

---
+++ examples/animal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "../ssal.c"
+
+class(Animal)
+class(Dog)
+
+interface(Animal
+  , method(eat, void ,(const *this) {
+    puts("Animal is eating...");
+  })
+
+  , method(sleep, void ,(const *this) {
+    puts("Animal is sleeping...");
+  })
+)
+
+interface(Dog, extends(Animal)
+  , method(bark, void ,(const *this) {
+    puts("Dog is barking!");
+  })
+)
+
+int main(void) {
+  Dog myDog;
+  Animal_eat(&myDog as Animal);
+  Animal_sleep(&myDog as Animal);
+  Dog_bark(&myDog);
+}
