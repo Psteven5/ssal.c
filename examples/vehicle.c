9fa409dbc35905c1f862fce9139b6f3b19ea7533
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-12-01 01:15:39 +0100

    Updates to use ts=4

--- examples/vehicle.c
+++ examples/vehicle.c
@@ -6,38 +6,38 @@ class(Vehicle)
 class(Car)
 
 interface(Vehicle
-  , virtual_method(accelerate, void ,(const *this), { assert(!"unreachable"); })
-  , virtual_method(brake, void ,(const *this), { assert(!"unreachable"); })
+    , virtual_method(accelerate, void ,(const *this), { assert(!"unreachable"); })
+    , virtual_method(brake, void ,(const *this), { assert(!"unreachable"); })
 
-  , method(startEngine, void ,(const *this) {
-    puts("Engine started!");
-  })
+    , method(startEngine, void ,(const *this) {
+        puts("Engine started!");
+    })
 )
 
 interface(Car, extends(Vehicle)
-  , method(accelerate, void ,(const *this) {
-    puts("Car: Pressing gas pedal...");
-  })
-  , static_method(_accelerate, void ,(const Vehicle *this) {
-    Car_accelerate(dynamic_cast(Vehicle, Car)(this));
-  })
+    , method(accelerate, void ,(const *this) {
+        puts("Car: Pressing gas pedal...");
+    })
+    , static_method(_accelerate, void ,(const Vehicle *this) {
+        Car_accelerate(dynamic_cast(Vehicle, Car)(this));
+    })
 
-  , method(brake, void ,(const *this) {
-    puts("Car: Applying brakes...");
-  })
-  , static_method(_brake, void ,(const Vehicle *this) {
-    Car_brake(dynamic_cast(Vehicle, Car)(this));
-  })
+    , method(brake, void ,(const *this) {
+        puts("Car: Applying brakes...");
+    })
+    , static_method(_brake, void ,(const Vehicle *this) {
+        Car_brake(dynamic_cast(Vehicle, Car)(this));
+    })
 
-  , static_property(vTable, static const Car_VTable, {
-    as Vehicle_VTable.accelerate = Car__accelerate,
-    as Vehicle_VTable.brake = Car__brake,
-  })
+    , static_property(vTable, static const Car_VTable, {
+        as Vehicle_VTable.accelerate = Car__accelerate,
+        as Vehicle_VTable.brake = Car__brake,
+    })
 )
 
 int main(void) {
-  Vehicle_Ptr myCar = {&(Car){} as Vehicle, &Car_vTable as Vehicle_VTable};
-  Vehicle_startEngine(myCar._);
-  myCar.v->accelerate(myCar._);
-  myCar.v->brake(myCar._);
+    Vehicle_Ptr myCar = {&(Car){} as Vehicle, &Car_vTable as Vehicle_VTable};
+    Vehicle_startEngine(myCar._);
+    myCar.v->accelerate(myCar._);
+    myCar.v->brake(myCar._);
 }
